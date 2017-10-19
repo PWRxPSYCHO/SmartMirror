025de5fdcc3c07a54b98e4853e843caf7a1c42cc
PWRxPSYCHO <32745243+PWRxPSYCHO@users.noreply.github.com>
2017-10-19 17:24:33 -0400

    Added Setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SmartMirror',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/PWRxPSYCHO/SmartMirror',
+    license='MIT License',
+    author='PWRxPSYCHO',
+    author_email='',
+    description=''
+)
